73f53fcd4e317366e6f33dde71f0fd3370908a5c
Risper <risper@networkdefense.eu>
2021-10-25 17:09:19 +0000

    jspractise2

--- script.js
+++ script.js
@@ -13,14 +13,38 @@ let percentage = actual / max;
 console.log(percentage);
 let mystring = 'The revolution will not be televised.';
 mystring;
-// let badString = string;
-// badString;
+console.log(mystring);
+let badString = mystring;
+badString;
+console.log(badString);
 
 
-// let sgl = 'Single quotes.';
-// let dbl = "Double quotes";
-// sgl;
-// dbl;
+let sgl = 'Would you eat a fish for supper?';
+console.log(sgl);
+let dbl = "I\'m feeling blue";
+console.log(dbl);
+sgl;
+dbl;
+let one = 'Hello, ';
+let two = 'how are you?';
+let joined = one + two;
+joined;
+console.log(joined);
+let multiple = one + one + one + one + two;
+multiple;
+console.log(multiple);
+let response = one + 'I am fine — ' + two;
+response;
+console.log(response);
+const button = document.querySelector('button');
+
+
+button onclick = function() {
+  let name = prompt('What is your name?');
+  alert('Hello ' + name + ', nice to see you!');
+  
+}
+
 
 // alert("hello!");
 // document.write("This is iwritten from javascript!"); 
