f7c3cd8798f362ffa698b55e2960c54637baf84b
Risper <risper@networkdefense.eu>
2021-10-25 15:21:00 +0000

    js practise1

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-let a = 5;
-let b =10;
-
+console.log("Hello world")
+console.log(23+97);
+console.log(1+2+3+4+5+6);
+console.log( (4+6+9) /77);
+let a =10;
 console.log(a);
+console.log(9*a);
+let b =7*a;
 console.log(b);
-console.log(a+b);
+let max = 57;
+let actual = (max -13);
+let percentage = actual / max;
+console.log(percentage);
+let mystring = 'The revolution will not be televised.';
+mystring;
+// let badString = string;
+// badString;
+
+
+// let sgl = 'Single quotes.';
+// let dbl = "Double quotes";
+// sgl;
+// dbl;
+
+// alert("hello!");
+// document.write("This is iwritten from javascript!"); 
