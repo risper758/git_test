9de95708243640bc0e976fc30c23a38f941c4113
Risper <risper@networkdefense.eu>
2021-10-27 15:09:30 +0000

    jspractise4

--- script.js
+++ script.js
@@ -39,23 +39,46 @@ console.log(response);
 
 let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 txt.length     // Returns 26
-alert( 2 > 1 );  // true (correct)
-alert( 2 == 1 ); // false (wrong)
-alert( 2 != 1 ); // true (correct)
-let result = 5 > 4; // assign the result of the comparison
-alert( result ); // true
-alert( 'Z' > 'A' ); // true
-alert( 'Glow' > 'Glee' ); // true
-alert( 'Bee' > 'Be' ); // true
-alert( '2' > 1 ); // true, string '2' becomes a number 2
-alert( '01' == 1 ); // true, string '01' becomes a number 1
-let a = 0;
-alert( Boolean(a) ); // false
+// alert( 2 > 1 );  // true (correct)
+// alert( 2 == 1 ); // false (wrong)
+// alert( 2 != 1 ); // true (correct)
+// let result = 5 > 4; // assign the result of the comparison
+// alert( result ); // true
+// alert( 'Z' > 'A' ); // true
+// alert( 'Glow' > 'Glee' ); // true
+// alert( 'Bee' > 'Be' ); // true
+// alert( '2' > 1 ); // true, string '2' becomes a number 2
+// alert( '01' == 1 ); // true, string '01' becomes a number 1
+// let a = 0;
+// alert( Boolean(a) ); // false
 
-let b = "0";
-alert( Boolean(b) ); // true
+// let b = "0";
+// alert( Boolean(b) ); // true
 
-alert(a == b); // true!
+// alert(a == b); // true!
+// alert( '' == false ); // true
+// alert( 0 == false ); // true
+// alert( 0 === false ); // false, because the types are different
+// alert( null === undefined ); // false
+// alert( null > 0 );  // (1) false
+// alert( null == 0 ); // (2) false
+// alert( null >= 0 ); // (3) true
+// alert( undefined > 0 ); // false (1)
+// alert( undefined < 0 ); // false (2)
+// alert( undefined == 0 ); // false (3)
+// 5 > 4 //true
+// "apple" > "pineapple" //false
+// "2" > "12" //true
+// undefined == null //true
+// undefined === null //false
+// null == "\n0\n" //false
+// null === +"\n0\n" //false
+const button = document.querySelector('button');
+
+button.onclick = function() {
+  let name = prompt('What is your name?');
+  alert('Hello ' + name + ', nice to see you!');
+}
 
 
 // alert("hello!");
