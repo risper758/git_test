3f39cbc258b065b4c938b63c321f6f2df44da302
Risper <risper@hinya.org>
2021-11-03 14:34:30 +0000

    rockpapergame

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 console.log("Hello world")
-console.log(23+97);
-console.log(1+2+3+4+5+6);
-console.log( (4+6+9) /77);
-let e =10;
-console.log(e);
-console.log(9*e);
-let f =7*e;
-console.log(f);
-let max = 57;
-let actual = (max -13);
-let percentage = actual / max;
-console.log(percentage);
-let mystring = 'The revolution will not be televised.';
-mystring;
-console.log(mystring);
-let badString = mystring;
-badString;
-console.log(badString);
+// console.log(23+97);
+// console.log(1+2+3+4+5+6);
+// console.log( (4+6+9) /77);
+// let e =10;
+// console.log(e);
+// console.log(9*e);
+// let f =7*e;
+// console.log(f);
+// let max = 57;
+// let actual = (max -13);
+// let percentage = actual / max;
+// console.log(percentage);
+// let mystring = 'The revolution will not be televised.';
+// mystring;
+// console.log(mystring);
+// let badString = mystring;
+// badString;
+// console.log(badString);
 
 
-let sgl = 'Would you eat a fish for supper?';
-console.log(sgl);
-let dbl = "I\'m feeling blue";
-console.log(dbl);
-sgl;
-dbl;
-let one = 'Hello, ';
-let two = 'how are you?';
-let joined = one + two;
-joined;
-console.log(joined);
-let multiple = one + one + one + one + two;
-multiple;
-console.log(multiple);
-let response = one + 'I am fine — ' + two;
-response;
-console.log(response);
+// let sgl = 'Would you eat a fish for supper?';
+// console.log(sgl);
+// let dbl = "I\'m feeling blue";
+// console.log(dbl);
+// sgl;
+// dbl;
+// let one = 'Hello, ';
+// let two = 'how are you?';
+// let joined = one + two;
+// joined;
+// console.log(joined);
+// let multiple = one + one + one + one + two;
+// multiple;
+// console.log(multiple);
+// let response = one + 'I am fine — ' + two;
+// response;
+// console.log(response);
 
-let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-txt.length     // Returns 26
+// let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// txt.length     // Returns 26
 // alert( 2 > 1 );  // true (correct)
 // alert( 2 == 1 ); // false (wrong)
 // alert( 2 != 1 ); // true (correct)
@@ -85,9 +85,9 @@ txt.length     // Returns 26
 
 // favoriteAnimal('Goat');
 // console.log(favoriteAnimal('Goat'));
-let myText = 'I am a string';
-let newString = myText.replace('string', 'sausage');
-console.log(newString);
+// let myText = 'I am a string';
+// let newString = myText.replace('string', 'sausage');
+// console.log(newString);
 // the replace() string function takes a source string,
 // and a target string and replaces the source string,
 // with the target string, and returns the newly formed string
@@ -143,18 +143,18 @@ console.log(newString);
 //     console.log(i);
 //   }
 // }
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+// let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
-for (let i = 1; i <= answer; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= answer; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 // alert("hello!");
 // document.write("This is iwritten from javascript!"); 
